a7dbe6d19453f5eb8423df25c3c15799ca46892d
Dustin Baker <DustinBakerSISpend@gmail.com>
2026-07-17 15:52:27 -0500

    Add sitemap and canonical URL policy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
-// @ts-check
+﻿// @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://tradeverdicts.com',
+  trailingSlash: 'always',
+  integrations: [sitemap()],
   server: {
     port: 4322
   }
